f8c22c3d2b89216a1d3a58b7c8e5656ffe6bc3b4
Zizwe <Zizwemkz@gmail.com>
2022-06-11 19:05:53 +0200

    first commit on my project on receiving message event

---
+++ MessageReceiver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    Port = 5672,
+                    VirtualHost = "/",
+                    UserName = "guest",
+                    Password = "guest"
+                };
+
+                using (var conn = factory.CreateConnection())
+                {
+                    using (var channel = conn.CreateModel())
+                    {
+                        channel.QueueDeclare("Department_1", true,false,false,null);
+                        Console.WriteLine(" [*] Waiting for messages from queue..");
+
+                        var consumer = new EventingBasicConsumer(channel);
+                        channel.BasicConsume("Department_1", true, consumer);
+
+                        consumer.Received += (model, get) =>
+                         {
+                             var rawMessage = get.Body.ToArray();
+                             var encodedMessage = Encoding.UTF8.GetString(rawMessage);
+                             Console.WriteLine("Received the following message: {0}", encodedMessage);
+
+                         };
+                       
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                    }
+                }
+            }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+        }
+    }
+}
