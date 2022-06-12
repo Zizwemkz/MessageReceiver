cb1a545cacbb001a9d1aed57235f3fe395ca0d51
Zizwe <Zizwemkz@gmail.com>
2022-06-12 20:58:53 +0200

    Code clean up.

---
+++ MessageReceiver/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageReceiver.Models
+{
+   public class Message
+    {
+       public string message { get; set; }
+        public string subject { get; set; }
+        public string date { get; set; }
+    }
+}
