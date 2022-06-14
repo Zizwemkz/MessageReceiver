9b1619825fab96f63562110cb24eecd45afa0f35
Zizwe <Zizwemkz@gmail.com>
2022-06-14 18:48:03 +0200

    code clean up with dependancy injection

--- MessageReceiver/Program.cs
+++ MessageReceiver/Program.cs
@@ -2,14 +2,29 @@
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using MessageReceiver.ReceiveEventService;
+using Microsoft.Extensions.DependencyInjection;
+using MessageReceiver.Service;
+using MessageReceiver.Configure;
+using MessageReceiver.Interface;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var container = Startup.ConfigureSerivce();
+        var _ReceiveService = container.GetService<IReceiver>();
 
-        var consumer = new Receiver();
-        var message = consumer.ReceiveEvent();
-        Console.WriteLine(" *** Waiting for messages from queue..");
-        Console.WriteLine("Received the following message: {0}", message);
+        var message = _ReceiveService.ReceiveEvent();
+        if (!message.Equals(" "))
+        {
+            Console.WriteLine(" *** Waiting for messages from queue..");
+            Console.WriteLine("Received the following message: {0}", message);
 
-
+        }
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
+
+    }
+    }
